8418698fd42b5cb12d24f7a0072ab81d8a25a8be
Florencia Lelli <104226407+florlelli@users.noreply.github.com>
2024-05-25 20:44:34 -0300

    Add files via upload

---
+++ solucionesPracticas/deDatos/Iniciando3.py
@@ -0,0 +1,9 @@
+# Ejercicio 3
+persona = {
+    "nombre": input("Ingrese el nombre: "),
+    "apellido": input("Ingrese el apellido: "),
+    "dni": input("Ingrese el DNI: "),
+    "email": input("Ingrese el email: "),
+    "fecha_nacimiento": input("Ingrese la fecha de nacimiento: ")
+}
+print("Datos de la persona:", persona)
