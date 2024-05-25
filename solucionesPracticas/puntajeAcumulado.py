1317710cf201d2ecbc9c90bcbcbfb63c3bff1334
Florencia Lelli <104226407+florlelli@users.noreply.github.com>
2024-05-25 18:23:04 -0300

    Delete solucionesPracticas/puntajeAcumulado.py

--- solucionesPracticas/puntajeAcumulado.py
+++
@@ -1,7 +0,0 @@
-partidos_ganados = int(input("Ingrese la cantidad de partidos ganados: "))
-partidos_empatados = int(input("Ingrese la cantidad de partidos empatados: "))
-partidos_perdidos = int(input("Ingrese la cantidad de partidos perdidos: "))
-
-puntos = (partidos_ganados * 3) + partidos_empatados
-
-print("El equipo tiene", puntos, "puntos en el campeonato.")
